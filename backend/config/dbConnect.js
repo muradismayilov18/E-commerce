0516cb9022d0eba8c532746d00cac29aa37bf332
muradismayilov18 <muradismayilov773@gmail.com>
2025-02-10 12:15:15 +0400

    backend

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -8,6 +8,5 @@ export const connectDatabase = () => {
     if (process.env.NODE_ENV === "PRODUCTION") DB_URI = process.env.DB_URI
 
     mongoose.connect(DB_URI).then((con) => {
-        console.log(`MongoDB bağlantısı başarıyla kuruldu ${con?.connection?.host}.`)
     })
 }
--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -4,22 +4,12 @@ import Product from "../model/Product.js";
 
 const seedProducts = async () => {
     try {
-        // Veritabanına bağlan
         await mongoose.connect("mongodb://localhost:27017/e-commerce");
-
-        // Eski ürünleri sil
         await Product.deleteMany();
-        console.log("Ürünler başarıyla silindi.");
-
-        // Yeni ürünleri ekle
         await Product.insertMany(products);
-        console.log("Ürünler başarıyla eklendi.");
-
-        // İşlemi sonlandır
         process.exit();
     } catch (err) {
-        console.error("Bir hata oluştu:", err);
-        process.exit(1); // Hata ile çıkış
+        process.exit(1); 
     }
 };
 
